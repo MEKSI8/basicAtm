64823e16cc674f362e28dc0891621bb9a9a937dc
MEKSI8 <109846389+MEKSI8@users.noreply.github.com>
2023-03-31 10:25:19 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int kalanHak = 3, balance = 1500;
+        int select;
+        String password, userName;
+        Scanner scan = new Scanner(System.in);
+
+        while (kalanHak > 0) {
+            System.out.print("Kullanıcı Adınız :");
+            userName = scan.nextLine();
+            System.out.print("Şifreniz Adınız :");
+            password = scan.nextLine();
+
+            if (userName.equals("patika") && password.equals("dev123")) {
+                System.out.println("Merhaba, Kodluyoruz Bankasına Hoş Geldiniz.");
+                do {
+
+                    System.out.println("1-Para Yatırma\n2-Para Çekme\n3-Bakiye Sorgu\n4-Çıkış Yap");
+                    System.out.println("Lütfen Yapmak İstediğiniz İşlemi Seçiniz :");
+                    select = scan.nextInt();
+
+                    switch (select) {
+                        case 1:
+                            System.out.print("Yatırılacak Tutar Giriniz :");
+                            int price = scan.nextInt();
+                            balance += price;
+                            System.out.println("İşlem Sonrası Bakiyeniz :" + balance);
+                            System.out.println("---------------");
+                            break;
+                        case 2:
+                            System.out.print("Para Çekmek İstediğiniz Tutarı Yazınız :");
+                            int girdi = scan.nextInt();
+                            if (girdi > balance) {
+                                System.out.print("Bakiye Yetersiz.");
+                            } else {
+                                balance -= girdi;
+                                System.out.println("Çekim Sonrası Bakiyeniz :" + balance);
+                            }
+                            System.out.println();
+
+                            break;
+                        case 3:
+                            System.out.print("Mevcut Bakiyeniz : " + balance);
+                            System.out.println();
+                            break;
+                        case 4:
+                            System.out.println("Tekrar görüşmek üzere.");
+
+                            break;
+                    }
+
+                } while (select != 4);
+                break;
+            } else {
+                kalanHak--;
+                System.out.println("Hatalı Girdiniz! Tekrar Deneyiniz!");
+                if (kalanHak == 0) {
+                    System.out.println("Hesabınız Bloke Oldu. Banka ile İletişime Geçin!");
+                } else {
+                    System.out.println("Kalan Hakkınız : " + kalanHak);
+                }
+            }
+
+        }
+    }
+}
